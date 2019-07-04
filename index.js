eea7c27eafde639694b54cbb53579054292b6529
Stuart Baker <stuart.baker@focusrite.com>
2019-07-04 11:02:34 +0100

    Respond with a random cricket fact

--- index.js
+++ index.js
@@ -1,10 +1,2 @@
-const Koa = require('koa');
-const app = new Koa();
-const PORT = process.env.PORT || 3000;
-
-app.use(async ctx => {
-    console.log(ctx);
-    ctx.body = 'Hello World';
-});
-
-app.listen(PORT);
+const app = require('./lib/app');
+app();
